4911bcfda02f1c79f188c4de3558e3e1f142828f
Andobas <juanhernandez@Juans-MacBook-Pro.local>
2016-01-26 17:51:01 -0800

    Added sizeToFit

--- MovieViewer/DetailViewController.swift
+++ MovieViewer/DetailViewController.swift
@@ -31,6 +31,8 @@ class DetailViewController: UIViewController {
         let overview = movie["overview"]
         overviewLabel.text = overview as? String
         
+        overviewLabel.sizeToFit()
+        
         let baseUrl = "http://image.tmdb.org/t/p/w500"
         
         if let posterPath = movie["poster_path"] as? String {
