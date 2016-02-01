5bfb66311ec18552e137c9398794a965ebd744c7
Andobas <juanhernandez@Juans-MacBook-Pro.local>
2016-01-31 17:46:53 -0800

    fixed loading low resolution image first for big poster

--- MovieViewer/DetailViewController.swift
+++ MovieViewer/DetailViewController.swift
@@ -35,19 +35,20 @@ class DetailViewController: UIViewController {
         let baseUrl = "http://image.tmdb.org/t/p/w500"
         
         
-        
-        
         //check if nil or not
-        if let posterPath = movie["poster_path"] as? String {
-            let imageUrl = NSURL(string: baseUrl + posterPath)
-            
+            let posterPath = movie["poster_path"] as? String
+            let imageUrl = NSURL(string: baseUrl + posterPath!)
+        
             //load low resolution image first then larger image
             let baseUrlSmall = "http://image.tmdb.org/t/p/w45"
-            let imageUrlSmall = NSURL(string: baseUrlSmall + posterPath)
-            let smallImageRequest = NSURLRequest(URL: imageUrlSmall!)
-            let largeImageRequest = NSURLRequest(URL: imageUrl!)
+            let baseUrlLarge = "https://image.tmdb.org/t/p/original"
             
-            posterImageView.setImageWithURLRequest(
+            //let imageUrlSmall = NSURL(string: baseUrlSmall + posterPath)
+            let smallImageRequest = NSURLRequest(URL: NSURL(string: baseUrlSmall + posterPath!)!)
+            let largeImageRequest = NSURLRequest(URL: NSURL(string: baseUrlLarge + posterPath!)!)
+            
+        
+            self.posterImageView.setImageWithURLRequest(
                 smallImageRequest,
                 placeholderImage: nil,
                 success: { (smallImageRequest, smallImageResponse, smallImage) -> Void in
@@ -56,46 +57,36 @@ class DetailViewController: UIViewController {
                     // in cache (might want to do something smarter in that case).
                     self.posterImageView.alpha = 0.0
                     self.posterImageView.image = smallImage;
-                    self.posterImageView!.contentMode = .ScaleAspectFit
                     
-                    UIView.animateWithDuration(2.0, animations: { self.posterImageView.alpha = 1.0},
-                        completion: { (sucess) -> Void in
+                    UIView.animateWithDuration(0.3, animations: { () -> Void in
+                        
+                        self.posterImageView.alpha = 1.0
+                        
+                        }, completion: { (sucess) -> Void in
                             
                             // The AFNetworking ImageView Category only allows one request to be sent at a time
                             // per ImageView. This code must be in the completion block.
-                            self.posterImageView!.setImageWithURLRequest(
+                            self.posterImageView.setImageWithURLRequest(
                                 largeImageRequest,
                                 placeholderImage: smallImage,
                                 success: { (largeImageRequest, largeImageResponse, largeImage) -> Void in
                                     
-                                    self.posterImageView!.image = largeImage;
+                                    self.posterImageView.image = largeImage;
                                     
                                 },
                                 failure: { (request, response, error) -> Void in
                                     // do something for the failure condition of the large image request
                                     // possibly setting the ImageView's image to a default image
-                                    //self.posterImageView!.image = UIImage(named: "MovieHolder")
                             })
                     })
                 },
                 failure: { (request, response, error) -> Void in
                     // do something for the failure condition
                     // possibly try to get the large image
-                    //self.posterImageView!.image = UIImage(named: "MovieHolder")
             })
-            
-            
-            
-            
-            posterImageView.setImageWithURL(imageUrl!)
-        }
-        
-        print(movie)
-        
-        
         // Do any additional setup after loading the view.
     }
-
+  
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
